ea808ea414d7a41614da7798623855ccb9bec2ac
fcbjimmy <jim.culers@gmail.com>
2023-08-10 23:51:04 +0800

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "./components/Providers";
 
 export const metadata: Metadata = {
   title: "Testing Blog with Next.js",
@@ -15,10 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-gray-100 text-black dark:bg-slate-900 dark:text-white">
+        <Providers>
+          <Navbar />
+          <main className="prose prose-xl prose-slate dark:prose-invert mx-auto">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
